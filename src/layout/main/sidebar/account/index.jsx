3afc069fdd334e05cfebdb018f2b82582f92daa9
Taha S. ASAN <tahaasann@gmail.com>
2024-01-24 14:03:23 +0300

    twitter-clone v0.0.6:002 : Profile button design is finished

--- src/layout/main/sidebar/account/index.jsx
+++ src/layout/main/sidebar/account/index.jsx
@@ -1,15 +1,20 @@
 import { Popover } from "@headlessui/react";
+import { useAccount } from "~/store/auth/hooks";
 
 export default function Account() {
+  const account = useAccount();
+
   return (
     <div className="mt-auto">
       <Popover>
-        <Popover.Button className="my-3 p-3 rounded-full hover:bg-[#eff3f41a] w-full flex">
-            test1
+        <Popover.Button className="my-3 p-3 rounded-full hover:bg-[#eff3f41a] w-full flex text-left items-center">
+          <img src={account.avatar} className="w-10 h-10 rounded-full" alt="" />
+          <div className="mx-3 text-[15px]">
+            <h6 className="font-bold">{account.fullName}</h6>
+            <div className="text-[#71767b]">@{account.username}</div>
+          </div>
         </Popover.Button>
-        <Popover.Panel>
-            acilacak yer!
-        </Popover.Panel>
+        <Popover.Panel>acilacak yer!</Popover.Panel>
       </Popover>
     </div>
   );
--- src/store/auth/hooks.js
+++ src/store/auth/hooks.js
@@ -1,4 +1,4 @@
 import { useSelector } from "react-redux";
-import { UseSelector } from "react-redux";
 
-export const useAccount = () => useSelector(state => state.a)
+
+export const useAccount = () => useSelector(state => state.auth.currentAccount)

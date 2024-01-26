4e78ec5323e3847fc763fab86ac490f66f37f5c0
Taha S. ASAN <tahaasann@gmail.com>
2024-01-26 16:59:26 +0300

    twitter-clone v0.0.6:005 : The update of the profile information button is not finished yet.

--- src/layout/main/sidebar/account/more/index.jsx
+++ src/layout/main/sidebar/account/more/index.jsx
@@ -17,13 +17,25 @@ export default function More() {
             <div className="text-[#71767b]">@{account.username}</div>
           </div>
           {currentAccount.id == account.id && (
-            <svg viewBox="0 0 24 24" width={18.75} height={18.75} fill="#00ba7c">
+            <svg viewBox="0 0 24 24" className="mr-2 ml-3" width={18.75} height={18.75} fill="#00ba7c">
               <path 
               d="M12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm-.81 14.68l-4.1-3.27 1.25-1.57 2.47 1.98 3.97-5.47 1.62 1.18-5.21 7.15z" />
             </svg>
           )}
         </button>
       ))}
+      <div className="h-px bg-[#2f3336] my-3" />
+      <button className="py-3 px-4 text-left hover:bg-[#eff3f41a] w-full transition-colors text-[#e7e9ea] text-[15px] font-bold leading-[20px]">
+            Var olan bir hesap ekle
+      </button>
+      <button className="py-3 px-4 text-left hover:bg-[#eff3f41a] w-full transition-colors text-[#e7e9ea] text-[15px] font-bold leading-[20px]">
+            Hesapları yönet
+      </button>
+      <button className="py-3 px-4 text-left hover:bg-[#eff3f41a] w-full transition-colors text-[#e7e9ea] text-[15px] font-bold leading-[20px]">
+          <div className="max-w-[228px]">
+          @tahaasann hesabından çıkış yap
+          </div>
+      </button>
     </div>
   )
 }

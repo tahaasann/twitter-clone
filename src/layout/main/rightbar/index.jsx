97a6bd9245f5086bf465c10c28fe93f89eabdcca
Taha S. ASAN <tahaasann@gmail.com>
2024-01-30 15:27:08 +0300

    twitter-clone v0.0.7 : Search button is being built

--- src/layout/main/rightbar/index.jsx
+++ src/layout/main/rightbar/index.jsx
@@ -1,7 +1,9 @@
+import Search from "~/layout/main/rightbar/search";
+
 export default function Rightbar(){
     return(
         <aside className="w-[350px] mr-2.5">
-            sag sidebar!
+            <Search />
         </aside>
     )
 }
--- src/layout/main/sidebar/account/index.jsx
+++ src/layout/main/sidebar/account/index.jsx
@@ -8,7 +8,7 @@ export default function Account() {
   return (
     <div className="mt-auto">
       <Popover className="relative">
-        <Popover.Button className="my-3 p-3 rounded-full hover:bg-[#eff3f41a] w-full flex text-left items-center outline-none">
+        <Popover.Button className="my-3 p-3 rounded-full hover:bg-[#eff3f41a] w-full flex text-left items-center transition-colors outline-none">
           <img src={account.avatar} className="w-10 h-10 rounded-full" alt="" />
           <div className="mx-3 text-[15px]">
             <h6 className="font-bold leading-[20px]">{account.fullName}</h6>
